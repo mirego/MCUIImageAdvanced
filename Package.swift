625445f6eb9eb0376e8c816e62481d007a952595
Florence Cimon-Paquet <fcimonpaquet@mirego.com>
2026-05-26 10:47:54 -0400

    bump to 12+

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "MCUIImageAdvanced",
     platforms: [
-        .iOS("9.0"),
-        .tvOS("9.0")
+        .iOS("12.0"),
+        .tvOS("12.0")
     ],
     products: [
         .library(
